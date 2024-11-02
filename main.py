77881795987418c9f3444f2ec593797160b723c0
JaseemProf <jaseem.bca@gmail.com>
2024-11-02 19:16:02 +0530

    Add: Add Category, Amount, and Date fields in the Expense page. #1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fasthtml.common import *
+from datetime import datetime
 
 
 app, route = fast_app(live=True)
@@ -12,8 +13,41 @@ def get():
 
 @route("/entry")
 def get():
-    back_button = A(Button("Back"), href="/")
-    return Titled("Add entry", back_button)
+    category_input = Select(
+        Option("Grocery", value="grocery"),
+        Option("Food", value="food"),
+        Option("Others", value="others"),
+        id="Category",
+    )
+
+    amount_input = Input(
+        placeholder="Amount", name="amount", type="number", required=True
+    )
+
+    # Date input field with current date as the default value
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    date_input = Input(placeholder="Date", name="date", type="date", value=current_date)
+
+    submit_button = A(Button("Submit", cls="outline"))
+
+    form = Form(
+        category_input,
+        amount_input,
+        date_input,
+        submit_button,
+        method="POST",
+        action="/add_entry",
+    )
+    return Titled(
+        "Add Entry",
+        form,
+    )
+
+
+@route("/add_entry")
+def post():
+    # TODO: handle the data
+    return P("Sucessfully added entry"), Meta(http_equiv="refresh", content="2; url=/")
 
 
 serve()
